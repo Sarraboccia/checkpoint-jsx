467d0b1c7f1f58e4e4077a9b5ffa21a241d95adc
Sara Boccia <saraboccia@Saras-MacBook-Air.local>
2021-10-26 23:10:45 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
   return (
     <div>
     <h1> Checkpoint jsx </h1>
+    
     </div>
+
   );
+  
 }
 
 export default App;
