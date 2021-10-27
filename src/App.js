b2203c62bbf7504fd884d7a466fcf0bfa87d15dd
Sara Boccia <saraboccia@Saras-MacBook-Air.local>
2021-10-27 12:44:12 +0100

    JSX update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-
-
-import './App.css';
+import "./App.css";
+import Image from "./Image.jpeg";
+import "./styles.css";
 
 function App() {
   return (
-    <div>
-    <h1> Checkpoint jsx </h1>
-    
-    </div>
+    <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+      <h1 className="title red">SARA</h1>
 
+      <img src={Image} className= "Photo" alt="" width="320" height="240"/>
+      <br />
+      <img src="MyImage.jpeg" className="Photo" alt="" width="320" height="240"/>
+      <br />
+      <video width="320" height="240" controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
+    </div>
   );
-  
 }
 
 export default App;
